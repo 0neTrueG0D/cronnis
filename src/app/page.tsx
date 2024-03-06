74687dcaa381532b668c3cb292a7cd3def841947
0neTrueG0D <mohamed@izhar.xyz>
2024-03-07 01:29:20 +0530

    Add Artworks component to display artwork images and audio players

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
+import Artworks from "./components/Artworks";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <Hero />
             <About />
             <Services />
+            <Artworks />
         </>
     );
 }
