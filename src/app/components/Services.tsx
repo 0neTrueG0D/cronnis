d564c563f32d00eb89da26b27729959d6e00595b
0neTrueG0D <mohamed@izhar.xyz>
2024-03-08 01:46:20 +0530

    Update Services component to include link and button updates

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 function Services() {
     return (
-        <div className="relative w-screen h-fit bg-gray-900 bg-opacity-75">
+        <div
+            id="services"
+            className="relative w-screen h-fit bg-gray-900 bg-opacity-75"
+        >
             <div className="max-w-6xl mx-auto h-full flex flex-col items-center p-5">
                 <h2 className="text-4xl md:text-6xl lg:text-7xl font-extrabold text-cyan-300 mt-16">
                     Services
@@ -50,9 +53,12 @@ function Services() {
                         </p>
                     </div>
                 </div>
-                <button className="my-16 py-3 px-10 text-cyan-300 border-2 border-cyan-300 uppercase text-lg font-semibold cursor-pointer hover:bg-cyan-300 hover:text-black duration-300">
+                <a
+                    href="#contact"
+                    className="my-16 py-3 px-10 text-cyan-300 border-2 border-cyan-300 uppercase text-lg font-semibold cursor-pointer hover:bg-cyan-300 hover:text-black duration-300"
+                >
                     Contact Me
-                </button>
+                </a>
             </div>
             <div className="absolute inset-0 bg-service bg-cover -z-10"></div>
         </div>
