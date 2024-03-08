5bea01e24edcfbbc1e6869b1059bc8f8ecfbf2dc
0neTrueG0D <mohamed@izhar.xyz>
2024-03-09 02:46:20 +0530

    Add Contact component and copyright footer to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Artworks from "./components/Artworks";
+import Contact from "./components/Contact";
 
 export default function Home() {
     return (
@@ -10,6 +11,12 @@ export default function Home() {
             <About />
             <Services />
             <Artworks />
+            <Contact />
+            <footer className="w-full bg-gray-900 text-cyan-50 text-lg flex items-center justify-center">
+                <p className="p-5">
+                    Copyright &copy; Arawn Cronnis 2024. All rights reserved.
+                </p>
+            </footer>
         </>
     );
 }
